da613190974c8dfc6efb3534b733386bdf9ef3c8
ronah289 <ronald.kibet@student.moringaschool.com>
2022-01-24 13:45:57 -0900

    bind views from activity_add_schedule.xml file to AddScheduleActivity

--- app/src/main/java/com/moringaschool/classschedulerapp/AddScheduleActivity.java
+++ app/src/main/java/com/moringaschool/classschedulerapp/AddScheduleActivity.java
@@ -2,13 +2,49 @@ package com.moringaschool.classschedulerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddScheduleActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddScheduleActivity extends AppCompatActivity implements View.OnClickListener{
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Title) EditText editScheduleTitle;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Location) EditText editScheduleLocation;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Description) EditText editScheduleDescription;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Start_Date) EditText editStartDate;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Start_Time) EditText editStartTime;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Finish_Date) EditText editEndDate;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.et_Add_Schedule_Finish_Time) EditText editEndTime;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.btn_Add_Schedule_Login) Button addScheduleButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
+        ButterKnife.bind(this);
+
+        addScheduleButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String scheduleTitle = editScheduleTitle.getText().toString();
+        if(view == addScheduleButton){
+            Toast.makeText(this, scheduleTitle, Toast.LENGTH_LONG).show();
+        }
     }
 }
