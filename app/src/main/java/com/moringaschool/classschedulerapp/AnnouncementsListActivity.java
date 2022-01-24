9487882009485735c42597dfc63950ab73a30bdd
ronah289 <ronald.kibet@student.moringaschool.com>
2022-01-24 13:53:21 -0900

    bind views from activity_announcements_list.xml file to AnnouncementsListActivity

--- app/src/main/java/com/moringaschool/classschedulerapp/AnnouncementsListActivity.java
+++ app/src/main/java/com/moringaschool/classschedulerapp/AnnouncementsListActivity.java
@@ -1,14 +1,37 @@
 package com.moringaschool.classschedulerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class AnnouncementsListActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AnnouncementsListActivity extends AppCompatActivity implements View.OnClickListener {
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.recyclerView2) RecyclerView recyclerView;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.button_add_note) FloatingActionButton addNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcements_list);
+        ButterKnife.bind(this);
+
+        addNoteButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == addNoteButton){
+            Toast.makeText(this, "add note button", Toast.LENGTH_LONG).show();
+        }
     }
 }
