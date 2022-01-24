44067f0d16b616f7ccf33ee844b97a0b21567ac3
ronah289 <ronald.kibet@student.moringaschool.com>
2022-01-24 14:29:52 -0900

    bind views from activity_schedule.xml file to ScheduleActivity

--- app/src/main/java/com/moringaschool/classschedulerapp/ScheduleActivity.java
+++ app/src/main/java/com/moringaschool/classschedulerapp/ScheduleActivity.java
@@ -1,14 +1,38 @@
 package com.moringaschool.classschedulerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class ScheduleActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ScheduleActivity extends AppCompatActivity implements View.OnClickListener{
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.recyclerViewSchedule) RecyclerView recyclerView;
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.button_add_note2) FloatingActionButton addNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        ButterKnife.bind(this);
+        addNoteButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == addNoteButton){
+            Toast.makeText(this, "schedule activity", Toast.LENGTH_SHORT).show();
+        }
     }
 }
