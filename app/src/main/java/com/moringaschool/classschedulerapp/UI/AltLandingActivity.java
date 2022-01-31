399a1ec6cf6d96492616fb63ae5ef6a7e45c6ad7
wdmuthiora <wdmuthiora@gmail.com>
2022-01-31 12:59:23 +0300

    Added navigation drawer toggle hamburger menu to AltLandingActivity.

--- app/src/main/java/com/moringaschool/classschedulerapp/UI/AltLandingActivity.java
+++ app/src/main/java/com/moringaschool/classschedulerapp/UI/AltLandingActivity.java
@@ -1,7 +1,10 @@
 package com.moringaschool.classschedulerapp.UI;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
@@ -11,7 +14,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AltLandingActivity extends AppCompatActivity {
-    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,20 @@ public class AltLandingActivity extends AppCompatActivity {
 
         //use custom toolbar as actionbar
         setSupportActionBar(toolbar);
-
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+
+    //don't leave activity on back press, but close drawer instead, if already open
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
